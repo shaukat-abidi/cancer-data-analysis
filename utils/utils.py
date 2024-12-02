1c07684397bb8650ed6a2e15e992f77aa8d639d0
shaukat_abedi@hotmail.com <shaukat_abedi@hotmail.com>
2024-12-03 00:28:06 +1100

    generated EDA reports in HTML for encounters, procedures, medications and patients

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,99 @@ tasks such as renaming and casting columns in a pandas DataFrame.
 """
 
 import pandas as pd
+from ydata_profiling import ProfileReport
+
+import yaml 
+
+def func_generate_report(df, filename_of_report):
+    # Calculate the size of the DataFrame in gigabytes
+    size_gb = df.memory_usage(deep=True).sum() / (1024**3)
+    print(f"Size of the DataFrame: {size_gb:.2f} GB")
+    # Create report
+    profile = ProfileReport(df, title="Report")
+    profile.to_file(filename_of_report)
+    
+
+def func_return_mappings(filename_yaml):
+    with open(filename_yaml, "r") as file:
+        dict_mappings = yaml.safe_load(file)
+    return dict_mappings
+
+def func_cast_columns(df, column_mappings):
+    """
+    Casts column types in a pandas DataFrame.
+
+    This function casts each column to its specified data type.
+
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        The input pandas DataFrame to process.
+
+    column_mappings : dict
+        A dictionary containing mappings for column names and types.
+        Example format:
+        {
+            "normalised_colname": {
+                "type": "str" | "float" | "datetimestamp"
+            }
+        }
+
+    Returns:
+    --------
+    pd.DataFrame
+        The modified pandas DataFrame with applied types.
+
+    Raises:
+    -------
+    KeyError:
+        If a column specified in `column_mappings` is not found in the DataFrame.
+    ValueError:
+        If a column cannot be cast to the specified type.
+
+    Notes:
+    ------
+    - For columns of type 'str', missing or NaN values are replaced with 'Unknown'.
+    - For columns of type 'datetimestamp', invalid dates are coerced to NaT.
+    - For columns of type 'float', invalid values are coerced to NaN.
+    """
+
+    # Ensure column_mappings is not empty
+    if not column_mappings:
+        raise ValueError("column_mappings cannot be empty.")
+
+    # Process each column based on the mappings
+    for original_col in df.columns.tolist():
+        if original_col not in column_mappings.keys():
+            print(f"{original_col} not found in column mappings")
+            continue  # Skip columns not in the mapping
+
+        try:
+            # Extract normalized column name and type
+            mapping = column_mappings[original_col]
+            col_type = mapping["type"]
+
+            # Handle type casting
+            if col_type == "str":
+                df[original_col] = df[original_col].fillna("Unknown").astype(str)
+                print(f"Column '{original_col}' cast to type 'str'.")
+            elif col_type == "datetimestamp":
+                df[original_col] = pd.to_datetime(df[original_col], errors="coerce").dt.tz_localize(None)
+                print(f"Column '{original_col}' cast to type 'datetimestamp'.")
+            elif col_type == "float":
+                df[original_col] = pd.to_numeric(df[original_col], errors="coerce")
+                print(f"Column '{original_col}' cast to type 'numeric'.")
+            else:
+                raise ValueError(f"Invalid type '{col_type}' for column '{original_col}'.")
+
+            print("-" * 20)
+
+        except KeyError as e:
+            raise KeyError(f"Column '{original_col}' is missing required mappings: {e}")
+        except Exception as e:
+            raise ValueError(f"Error processing column '{original_col}': {e}")
+
+    return df
 
 def func_rename_and_cast_columns(df, column_mappings):
     """
